861ee975c4871fa818cb94726d4eabce953b9855
BabyManeesha Sunkara <babymaneeshasunkara@BabyManeeshas-MacBook-Pro.local>
2020-04-04 19:11:46 +0530

    Added MongoDB and Mongoose Support

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 require("dotenv").config()
 
+// Models
+const Todo = require("./models/todo");
+const DefaultTodos = require("./models/defaultTodos");
+
 const app = express();
+
+mongoose.connect(process.env.MONGOURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let msg = "Hello World!";
+
+Todo.find({}, (err, todos) => {
+    if(err){
+        console.log("Something went wrong!!", err);
+    }else if(todos && todos.length <=0) {
+        Todo.insertMany(DefaultTodos, (err, newtodos) => {
+            if(err){
+                console.log("Something went wrong while initiating default todos!!", err);
+            }else{
+                console.log("Initiated ToDos");
+            }
+        });
+    }
+});
+
 
 app.get('/', (req, res) => {
-    res.render('home', {msg: msg});
+    Todo.find({}, (err, todos) => {
+        if(err){
+            todos = [];
+            console.log("Something wentt wrong while getting Todos");
+        }else{
+            res.render('home', {todos: todos});
+        }
+    });
 });
 
 app.post('/', (req, res) => {
-    msg = req.body.name;
-    res.render('home', {msg: msg});
+    const todo = new Todo({ todo: req.body.newItem})
+    todo.save((err, newtodo) => {
+        res.redirect('/');
+    });
+});
+
+app.post('/delete', (req, res) => {
+    const id = req.body.id;
+    Todo.findByIdAndDelete({_id: id}, (err, todo) => {
+        if(err){
+            console.log("Error while deleting", err);
+        }else{
+            res.redirect('/');
+        }
+    })
 });
 
 app.listen(process.env.PORT || 3000, () => {
---
+++ models/defaultTodos.js
@@ -0,0 +1,8 @@
+const DefaultTodos = [
+    { todo: 'Wake Up'},
+    { todo: 'Eat'},
+    { todo: 'Code'},
+    { todo: 'Sleep'}
+];
+
+module.exports = DefaultTodos;
